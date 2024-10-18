09a45bba5dec3ba30de085d58662f8fb22957609
paginated <paginated.git@protonmail.com>
2024-10-18 15:41:30 +0530

    dotfiles: hm: foot: Set font family to SauceCodePro Nerd Font

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -5,6 +5,14 @@
     settings = {
       main = {
         term = "xterm-256color";
+
+        font = "SauceCodePro Nerd Font:size=10.2:style=SemiBold";
+        font-bold = "SauceCodePro Nerd Font:size=10.2:style=Bold";
+        font-italic = "SauceCodePro Nerd Font:size=10.2:style=Italic";
+        font-bold-italic = "SauceCodePro Nerd Font:size=10.2:style=Bold,Italic";
+        dpi-aware = true;
+        line-height = 19;
+        underline-offset = 19;
       };
     };
   };
