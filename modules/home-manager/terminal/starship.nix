b7445df1cb09455928b62a2c6e348d7ca3f6b7e3
paginated <paginated.git@protonmail.com>
2024-10-18 16:43:59 +0530

    dotfiles: hm: starship: Set git status symbols

--- modules/home-manager/terminal/starship.nix
+++ modules/home-manager/terminal/starship.nix
@@ -16,6 +16,20 @@
         format = "on [$symbol $branch(:$remote_branch)]($style) ";
         symbol = "";
       };
+      git_status = {
+        format = "([\\[ $all_status$ahead_behind\\]]($style) )";
+        style = "bold purple";
+        ahead = "[ $count](bold blue) ";
+        behind = "[ $count](bold red) ";
+        staged = "[ $count](bold green) ";
+        deleted = "[ $count](bold red) ";
+        renamed = "[ $count](bold green) ";
+        stashed = "[ $count](bold red) ";
+        diverged = "[ $count](bold red) ";
+        modified = "[ $count](bold yellow) ";
+        untracked = "[ $count](bold yellow) ";
+        conflicted = "[󰞇 $count](bold red) ";
+      };
     };
   };
 }
